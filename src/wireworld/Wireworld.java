ae3ab030286f539fbe251b25b26b9e2bbb88bbd8
mikub97 <mikub97@gmail.com>
2017-05-29 23:49:16 +0200

    Cellgrid serializable

--- src/wireworld/Wireworld.java
+++ src/wireworld/Wireworld.java
@@ -16,6 +16,7 @@ public class Wireworld {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new WireStartGUI();
             }
